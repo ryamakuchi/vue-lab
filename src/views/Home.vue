160e790db3a01a1353e05f2d5c2a4edf1c5494ee
ryamakuchi <r.yamakuchi@gmail.com>
2019-06-23 15:56:05 +0900

    Exercise 2: リストレンダリング

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,25 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld :msg="message"/>
+
+    <h3>リストレンダリング</h3>
+    <ul id="item-list">
+      <li v-for="item in items" :key="item.id">
+        {{item.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+// ここにインターフェースを追加
+declare interface Item {
+  id: number;
+  name: string;
+}
 
 export default Vue.extend({
   name: 'home',
@@ -16,7 +29,17 @@ export default Vue.extend({
   },
   data() {
     return {
-      message: 'Hello Vue.js!'
+      message: 'Hello Vue.js!',
+      items: [
+        {
+          id: 1,
+          name: "キーボード"
+        },
+        {
+          id: 2,
+          name: "マウス"
+        }
+      ] as Item[]
     }
   }
 });
